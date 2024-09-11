1c1e3f107079e689d2d041f8c4fac3ba45f7611e
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-09-11 15:51:25 +1200

    Updating loginPage and RegisterPage. register page allow to signup and store the password which is encryted. loginPage allows to login in correct password and email then if success login, redirect to homepage which does not have login and register navigator. Also the navbar is changed to represent without login and register value after passing the login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import JobDetailsPage from './pages/JobDetailsPage';
@@ -8,19 +8,33 @@ import RegisterPage from './pages/RegisterPage';
 import CompanyReviewPage from './pages/CompanyReviewPage';
 import Navbar from './components/Navbar';
 
+  
 function App() {
+  
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+    const handleLogout = () => {
+      // Simply set isAuthenticated to false on logout
+      setIsAuthenticated(false);
+    };
+  
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/job/:id" element={<JobDetailsPage />} />
-        <Route path="/applications" element={<ApplicationsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<RegisterPage />} />
-        <Route path="/company-reviews" element={<CompanyReviewPage />} />
-      </Routes>
-    </Router>
+     <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/job/:id" element={<JobDetailsPage />} />
+      <Route path="/applications" element={<ApplicationsPage />} />
+      <Route path="/company-reviews" element={<CompanyReviewPage />} />
+      {/* Conditionally render the Login and Register routes */}
+      {!isAuthenticated ? (
+        <>
+          <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/signup" element={<RegisterPage />} />
+        </>
+      ) : null}
+    </Routes>
+  </Router>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ isAuthenticated, handleLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -20,12 +20,24 @@ function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" to="/company-reviews">Company Reviews</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">Register</Link>
-            </li>
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <button className="btn btn-link nav-link" onClick={handleLogout}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">Register</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
