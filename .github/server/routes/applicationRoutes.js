17717f6d3973b9cb579349df85835c42a9c78e9c
ljld12315 <134265939+ljld12315@users.noreply.github.com>
2024-09-16 22:55:41 +1200

    cleaned up codes
    no more mongodb elements

--- .github/server/routes/applicationRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { getApplications, applyToJob } = require('../controllers/applicationController');
-
-router.get('/', getApplications);
-router.post('/apply', applyToJob);
-
-module.exports = router;
--- server/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getJobs, getJobById, createJobListing } = require('../controllers/jobController');
-const router = express.Router();
-
-router.get('/', getJobs); // Get all jobs from the database
-router.get('/:id', getJobById); // Get a job by ID from the database
-router.post('/', createJobListing); // Post a new job to the database
-
-module.exports = router;
